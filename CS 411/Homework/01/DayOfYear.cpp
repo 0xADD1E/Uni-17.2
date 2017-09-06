35a4738ad6a3e3212ceaedf661394c92677630aa
Kate Morrison <a9rkzz@gmail.com>
2017-09-06 15:23:42 -0500

    Complete :P

--- CS 411/Homework/01/DayOfYear.cpp
+++ CS 411/Homework/01/DayOfYear.cpp
@@ -8,17 +8,17 @@ DayOfYear::DayOfYear() : DayOfYear(1) {}
 
 int DayOfYear::monthLengths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-void DayOfYear::input() {
+std::istream& operator>>(std::istream& is, DayOfYear& doy){
   int month = 1, day = 1;
-  std::cout << "Enter the month as a number: ";
-  std::cin >> month;
-  std::cout << "Enter the day of the month: ";
-  std::cin >> day;
-  setDayOfYear(month, day);
+  is >> month;
+  is.get();
+  is >> day;
+  doy.dayOfYear = doy.setDayOfYear(month, day);
+  return is;
 }
 
-void DayOfYear::output( ){
-  int month = getMonth();
+std::ostream& operator<<(std::ostream& os, const DayOfYear& doy){
+  int month = doy.getMonth();
   std::string monthName;
   switch (month){
     case 1:
@@ -48,7 +48,8 @@ void DayOfYear::output( ){
     default:
     monthName = "Error in DayOfYear::output.";
   }
-  std::cout << monthName << " " << getDay();
+  std::cout << monthName << " " << doy.getDay();
+  return os;
 }
 
 int DayOfYear::setDayOfYear(int month, int day){
@@ -62,21 +63,103 @@ int DayOfYear::setDayOfYear(int month, int day){
   //in python this would be `return sum(monthLengths[:month-1])+day`
   return acc;
 }
-int DayOfYear::getMonth(){
+int DayOfYear::getMonth() const{
   int dayAcc = dayOfYear;
-  int monthAcc = 1;
+  int monthAcc = 0;
   while(dayAcc > 31){
     dayAcc -= monthLengths[monthAcc];
     monthAcc++;
   }
-  return monthAcc;
+  return ++monthAcc;
 }
-int DayOfYear::getDay(){
+int DayOfYear::getDay() const{
   int dayAcc = dayOfYear;
-  int monthAcc = 1;
+  int monthAcc = 0;
   while(dayAcc > 31){
     dayAcc -= monthLengths[monthAcc];
     monthAcc++;
   }
   return dayAcc;
+}
+
+const DayOfYear DayOfYear::operator+(const DayOfYear& doy){
+  DayOfYear nDay = DayOfYear();
+  nDay.dayOfYear = (dayOfYear+doy.dayOfYear)%365;
+  return nDay;
+}
+const DayOfYear DayOfYear::operator+(const int& numOfDays){
+  DayOfYear nDay = DayOfYear();
+  nDay.dayOfYear = (dayOfYear+numOfDays)%365;
+  return nDay;
+}
+const DayOfYear DayOfYear::operator-(){
+  DayOfYear nDay = DayOfYear();
+  nDay.dayOfYear = 365-nDay.dayOfYear;
+  return nDay;
+}
+const bool DayOfYear::operator==(const DayOfYear& doy){
+  return dayOfYear == doy.dayOfYear;
+}
+const bool DayOfYear::operator>(const DayOfYear& doy){
+  return dayOfYear > doy.dayOfYear;
+}
+const bool DayOfYear::operator<(const DayOfYear& doy){
+  return dayOfYear < doy.dayOfYear;
+}
+const DayOfYear& DayOfYear::operator++(){
+  //Prefix Operator
+  dayOfYear++;
+  dayOfYear %= 365;
+  return *this;
+}
+const DayOfYear DayOfYear::operator++(int){
+  DayOfYear oDay = DayOfYear();
+  oDay.dayOfYear = dayOfYear;
+  dayOfYear++;
+  dayOfYear %= 365;
+  return oDay;
+}
+const DayOfYear& DayOfYear::operator--(){
+  //Prefix Operator
+  dayOfYear--;
+  dayOfYear %= 365;
+  return *this;
+}
+const DayOfYear DayOfYear::operator--(int){
+  DayOfYear oDay = DayOfYear();
+  oDay.dayOfYear = dayOfYear;
+  dayOfYear--;
+  dayOfYear %= 365;
+  return oDay;
+}
+const int DayOfYear::operator[](const int index){
+  switch(index){
+    case 1: return getMonth();
+    case 2: return getDay();
+    case 3: return dayOfYear;
+    default: return -1;
+  }
+}
+
+const DayOfYear operator -(const DayOfYear& doy1, const DayOfYear& doy2){
+  //Ugly bc not allowed to be member function
+  int day1 = doy1.getDay(), day2 = doy2.getDay();
+  int month1 = doy1.getMonth(), month2 = doy2.getMonth();
+  int dayCount = day1 - day2 + 365;
+  int monthLengths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  for(int i = 0; i<month1-1; i++){
+    dayCount += monthLengths[i];
+  }
+  for(int i = 0; i<month2-1; i++){
+    dayCount -= monthLengths[i];
+  }
+
+  dayCount %= 365;
+
+  int monthCount = 0;
+  while(dayCount>31){
+    dayCount -= monthLengths[monthCount];
+    monthCount ++;
+  }
+  return DayOfYear(++monthCount, dayCount);
 }
--- CS 411/Homework/01/main.cpp
+++ CS 411/Homework/01/main.cpp
@@ -1,14 +1,34 @@
-#include <iostream>
 #include "DayOfYear.hpp"
 
 int main( ) {
-	DayOfYear date1(2, 28), date2(12, 31), date3;
-  std::cout << "Initialized dates:\n";
-  date1.output( ); std::cout << std::endl;
-  date2.output( ); std::cout << std::endl;
-  date3.output( ); std::cout << std::endl;
-  date1 = DayOfYear(10, 32);
-  std::cout << "date1 reset to the following:\n";
-  date1.output( ); std::cout << std::endl;
+	DayOfYear d1 = DayOfYear();
+	DayOfYear d2 = DayOfYear(2);
+	DayOfYear d3 = DayOfYear(3, 5);
+	DayOfYear d4 = DayOfYear(8, 13);
+
+  std::cout << d1<<std::endl;
+	std::cout << d2<<std::endl;
+	std::cout << (d1 + d2) << std::endl;
+	std::cout << (d4 + d4) << std::endl;
+	std::cout << (d1 + 1) << std::endl;
+	std::cout << (d4 + 200) << std::endl;
+	std::cout << (d4 - d3) << std::endl;
+	std::cout << (d1 - d4) << std::endl;
+	std::cout << (-d2) << std::endl;
+	std::cout << (d1 == d1) << std::endl;
+	std::cout << (d1 == d2) << std::endl;
+	std::cout << (d1 < d2) << std::endl;
+	std::cout << (d2 < d1) << std::endl;
+	std::cout << (d1 > d2) << std::endl;
+	std::cout << (d2 > d1) << std::endl;
+  std::cout << (++d1) << std::endl;
+	std::cout << (d2++) << std::endl;
+	std::cout << (d2++) << std::endl;
+	std::cout << (--d1) << std::endl;
+	std::cout << (d2--) << std::endl;
+	std::cout << (d2--) << std::endl;
+	std::cout << (d4[1]) << std::endl;
+	std::cout << (d4[2]) << std::endl;
+	std::cout << (d4[3]) << std::endl;
  	return 0;
 }
