2c3ea5da3adc6d74603d0446a42d9d50a5e1d51f
Kate Morrison <a9rkzz@gmail.com>
2017-11-28 09:04:34 -0600

    Actually doing the recursive function again...

--- CS 238/Homework/HW 8/HW 8.asm
+++ CS 238/Homework/HW 8/HW 8.asm
@@ -9,72 +9,49 @@ INCLUDE io.h
 .STACK 4096
 
 .DATA
-number1 DWORD   ?
-number2 DWORD   ?
 prompt1 BYTE    "Enter A", 0
 prompt2 BYTE    "Enter B", 0
-string  BYTE    40 DUP (?)
+string  BYTE    11 DUP (?)
 resLbl  BYTE    "GCD(A,B) is", 0
 result  BYTE    11 DUP (?), 0
 
 .CODE
 gcd PROC
-        push    ebp
+        push    ebp             ; Stack Frame Things
         mov     ebp, esp
+        push    esi
+        push    edi
+        mov     edi, [ebp + 8]
+        mov     esi, [ebp + 12]
 
-        mov     esi, [ebp + 8]
-        mov     edi, [ebp + 12]
         cmp     esi, edi
         ja      more
         jb      less
         mov     eax, esi
-	pop     ebp
-        ret     8
+        jmp     done
 more:   sub     esi, edi
-	push    esi
-	push    edi
-        call    gcd
-	pop     ebp
-        ret     8
+	jmp     recurse
 less:   sub     edi, esi
-	push    esi
-	push    edi
+recurse:push    esi
+        push    edi
         call    gcd
+done:   pop     edi             ; Stack Frame Cleanup
+        pop     esi
+        mov     esp, ebp
         pop     ebp
-        ret     8
+	ret     8
 gcd ENDP
-gcdfast PROC
-        cmp     esi, edi
-        ja      more
-        jb      less
-        mov     eax, esi
-        ret
-more:   sub     esi, edi
-        call    gcd
-        ret
-less:   sub     edi, esi
-        call    gcd
-        ret
-gcdfast ENDP
 _MainProc PROC
         input   prompt1, string, 40
         atod    string
-        mov     esi, eax
+        mov     ebx, eax
 
         input   prompt2, string, 40
         atod    string
-        mov     edi, eax
 
-        ; Standard, boring, complicated implementation with C style calling
-        push    esi
-        push    edi
+        push    ebx
+        push    eax
         call    gcd
-        
-        ; Alternative implementation with the fastcall convention.
-        ; Uses registers instead of the stack. Faster, less risk
-        ; (though only useful for things that fit into 6 registers)
-        ; Which would you rather see in your code?
-        call gcdfast
 
         dtoa    result, eax
         output  resLbl, result
