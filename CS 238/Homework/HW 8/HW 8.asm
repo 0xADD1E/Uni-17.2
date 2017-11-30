dc6c080e0797760af753eee559ce0d273aa965ae
Kate Morrison <a9rkzz@gmail.com>
2017-11-29 18:05:47 -0600

    Comments are gr8

--- CS 238/Homework/HW 8/HW 8.asm
+++ CS 238/Homework/HW 8/HW 8.asm
@@ -18,28 +18,28 @@ result  BYTE    11 DUP (?), 0
 .CODE
 gcd PROC
         push    ebp             ; Stack Frame Things
-        mov     ebp, esp
-        push    esi
-        push    edi
-        mov     edi, [ebp + 8]
-        mov     esi, [ebp + 12]
+        mov     ebp, esp	; Set the base pointer to track the stack frame for this function
+        push    esi		; Backup ESI and EDI
+        push    edi		; (since we're using them here)
+        mov     edi, [ebp + 8]	; Skip over the push ebp and implicit push esi from the call (param 2)
+        mov     esi, [ebp + 12]	; param 1
 
-        cmp     esi, edi
-        ja      more
-        jb      less
-        mov     eax, esi
-        jmp     done
-more:   sub     esi, edi
-	jmp     recurse
-less:   sub     edi, esi
-recurse:push    esi
-        push    edi
-        call    gcd
-done:   pop     edi             ; Stack Frame Cleanup
-        pop     esi
-        mov     esp, ebp
-        pop     ebp
-	ret     8
+        cmp     esi, edi	; Comparison thing
+        ja      more		; A > B
+        jb      less		; A < B
+        mov     eax, esi	; A = B
+        jmp     done		; if A=B, we're done and can ret
+more:   sub     esi, edi	; A-=B
+	jmp     recurse		; and then call gcd again with the new value of a
+less:   sub     edi, esi	; B-=A
+recurse:push    esi		; Push A and B
+        push    edi		; onto the stack as params for the new call
+        call    gcd		; (do the call thing)
+done:   pop     edi             ; Restore ESI and EDI
+        pop     esi		; (from any changes we made to them
+        mov     esp, ebp	; Restore the stack frame to where it was
+        pop     ebp		; Enter the old stack frame
+	ret     8		; And ret, removing the two params from the stack (esp+=8 after the ret)
 gcd ENDP
 _MainProc PROC
         input   prompt1, string, 40
